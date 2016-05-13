1c37f1609794e05ee07efc1ee93eaae1ec72c55b
xiaochen <tanxc1989@gmail.com>
2016-05-13 16:01:22 +0800

    up

--- collector.js
+++ collector.js
@@ -3,34 +3,57 @@
  */
 var Request = require('request');
 var Cheerio = require('cheerio');
+var Url = require('url');
 var Proxy = require('./proxy');
 
 function Collector(config) {
     this.config = config;
+    this.detailPages = [];
 }
 
 Collector.prototype.run = function () {
     var self = this;
     var config = this.config;
     var urls = config.url;
+    var total = urls.length;
+    var doneCount = 0;
     if (!Array.isArray(urls)) {
         return;
     }
     urls.forEach(function (url) {
         setTimeout(function () {
-            self.request(url);
+            self.request(url, function (body) {
+                self.dataParse(body);
+                if (self.allCompleted(++doneCount, total)) {
+                    self.fetchDetail();
+                }
+            });
         }, config.delay);
+    });
+};
+
+Collector.prototype.allCompleted = function (doneCount, total) {
+    return doneCount >= total;
+};
+
+Collector.prototype.fetchDetail = function () {
+    var self = this;
+
+    this.detailPages.forEach(function (url, index) {
+        self.request(url, function (body) {
+            self.config.complete(body, index);
+        });
     });
 
 };
 
-Collector.prototype.request = function (dataUrl) {
+Collector.prototype.request = function (dataUrl, done) {
     Request({
         url: dataUrl,
         gzip: true,
         proxy: this.config.proxy ? Proxy.get() : '',
         headers: {
-            'User-Agent': 'a'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
         }
     }, function (err, res, body) {
         if (err) {
@@ -39,18 +62,17 @@ Collector.prototype.request = function (dataUrl) {
             return;
         }
 
-        this.dataParse(body);
+        done && done(body);
     }.bind(this));
 };
 
 Collector.prototype.dataParse = function (body) {
     var $ = Cheerio.load(body);
-    var $list = $(this.config.rule);
-
-    console.log('===start collecting===');
+    var $list = $(this.config.ruleList);
 
     $list.each(function (index, item) {
-        this.config.complete($(item), index);
+        this.detailPages.push(Url.resolve(this.config.url[0], $(item).attr('href')));
+        //this.config.complete($(item), index);
     }.bind(this));
 };
 
--- index.js
+++ index.js
@@ -2,15 +2,18 @@
  * Created by xiaochen on 2016/5/9.
  */
 var Collector = require('./collector');
+var Cheerio = require('cheerio');
 
 var config = {
     url: ['http://www.bilibili.com/video/movie.html'],
     //url: ['https://api.github.com/repos/request/request'],
-    rule: '.v-list.sub li a .t',
+    ruleList: '.v-list.sub li a',
     delay: 100,
     proxy: false,
-    complete: function ($item, index) {
-        var title = $item.text();
+    complete: function (body, index) {
+        var $ = Cheerio.load(body);
+        var title = $('title').text();
+
         console.info(index + ':' + title);
     }
 };
